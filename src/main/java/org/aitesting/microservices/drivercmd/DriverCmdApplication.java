2fe0660a26299d9601231c5cc7d2386fc5610621
Jacob Kissel <jacob_kissel@ultimatesoftware.com>
2018-06-26 11:52:10 -0400

    Added eureka to docker-compose, enabled eureka for application

--- src/main/java/org/aitesting/microservices/drivercmd/DriverCmdApplication.java
+++ src/main/java/org/aitesting/microservices/drivercmd/DriverCmdApplication.java
@@ -2,7 +2,9 @@ package org.aitesting.microservices.drivercmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DriverCmdApplication {
     public static void main(String[] args) {
